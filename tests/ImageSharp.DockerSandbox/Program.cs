967a44529b985256d87478628012263b88bb2bcc
Brian Popow <popow@gmx.de>
2019-05-30 17:30:11 +0200

    Add missing Program.cs

---
+++ tests/ImageSharp.DockerSandbox/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+
+namespace DockerSandbox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var carImage = Image.Load<Rgba32>(@"Car.jpg");
+            using (var fs = new FileStream("/app/docker_output/car_output.jpg", FileMode.Create))
+            {
+                carImage.SaveAsJpeg(fs);
+                fs.Flush();
+                fs.Close();
+            }
+
+            var lakeImage = Image.Load<Rgba32>(@"Lake.jpg");
+            using (var fs = new FileStream("/app/docker_output/lake_output.jpg", FileMode.Create))
+            {
+                lakeImage.SaveAsJpeg(fs);
+                fs.Flush();
+                fs.Close();
+            }
+
+            Console.WriteLine("output written to docker_output");
+        }
+    }
+}
